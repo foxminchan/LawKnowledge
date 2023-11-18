250c3b7996728a0b7443c7f6ca2722d3dd2009c7
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-11-18 12:10:48 +0700

    feat: add scraping

--- apps/auto/Program.cs
+++ apps/auto/Program.cs
@@ -1,23 +1,11 @@
-var builder = WebApplication.CreateSlimBuilder(args);
+using LawKnowledge.Auto;
 
+var builder = WebApplication.CreateSlimBuilder(args);
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
-
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+app.MapGet( "/", () =>
+  new Scraping().DataProcessing()
+);
 
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
---
+++ apps/auto/Scraping.cs
@@ -0,0 +1,64 @@
+using HtmlAgilityPack;
+using System.Net;
+using System.Text;
+
+namespace LawKnowledge.Auto;
+
+public class Scraping
+{
+  private readonly HtmlDocument _htmlDocument = new();
+  private readonly ILogger<Scraping> _logger = new Logger<Scraping>(new LoggerFactory());
+
+  private List<string> GetData(string filePath)
+  {
+    filePath = Uri.UnescapeDataString(filePath);
+
+    if (!File.Exists(filePath))
+    {
+      _logger.LogError("{file} not found: {path}", nameof(File), filePath);
+      return new();
+    }
+
+    _htmlDocument.LoadHtml(File.ReadAllText(filePath, Encoding.UTF8));
+    return GetBodyStr(_htmlDocument.DocumentNode.Descendants().Skip(1).ToList());
+  }
+
+  private static List<string> GetBodyStr(IEnumerable<HtmlNode> htmlNodes)
+    => htmlNodes
+      .Where(node => !node.HasChildNodes && !string.IsNullOrWhiteSpace(node.InnerText))
+      .Select(node => WebUtility.HtmlDecode(node.InnerText).Trim())
+      .Select(text =>
+      {
+        var sb = new StringBuilder(text)
+          .Replace("\n\n", "\n")
+          .Replace("\t\t", "\t")
+          .Replace("\r\r", "\r")
+          .Replace("…", "")
+          .Replace("(", "")
+          .Replace(")", "")
+          .Replace(";", "");
+
+        return sb.ToString();
+      })
+      .Where(text => !string.IsNullOrEmpty(text) &&
+                     !text.StartsWith("{") &&
+                     text.Length > 1 &&
+                     text != "\n" && text != "\r" && text != "\t" &&
+                     text != "." && text != "," && text != "|")
+      .ToList();
+
+  public void DataProcessing()
+  {
+    const string outputDirectory = "Output";
+
+    if (!Directory.Exists(outputDirectory))
+      Directory.CreateDirectory(outputDirectory);
+
+    foreach (var filePath in Directory.GetFiles(Uri.UnescapeDataString("Raw/demuc"), "*.html"))
+    {
+      var outputPath = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(filePath) + ".txt");
+      File.WriteAllLines(outputPath, GetData(filePath));
+      _logger.LogInformation("Output: {path}", outputPath);
+    }
+  }
+}
