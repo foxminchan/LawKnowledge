b41d4466ecffc5c65c5df47c142d2727663b34aa
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-11-23 22:31:06 +0700

    feat: init item search detail

---
+++ apps/website/src/features/ItemSearchDetail/index.tsx
@@ -0,0 +1,38 @@
+import useMetadata from '@/common/hooks/useMetadata';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Breadcrumbs, Container, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  title: string;
+};
+
+export default function OnlineService(props: Readonly<Props>) {
+  useMetadata(props.title);
+
+  const breadcrumbs = [
+    <Link key="1" color="inherit" to="/">
+      Trang chủ
+    </Link>,
+    <Typography key="2" color="text.primary">
+      Văn bản pháp luật
+    </Typography>,
+    <Typography key="3" color="text.primary">
+      Chi tiết văn bản
+    </Typography>,
+  ];
+
+  return (
+    <main className="min-h-[calc(100vh_-_400px)]">
+      <Container>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+          className="py-5"
+        >
+          {breadcrumbs}
+        </Breadcrumbs>
+      </Container>
+    </main>
+  );
+}
