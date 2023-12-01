d10d5ffde1164ac143ca791a44a3d66a6c3f6bae
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-12-01 17:08:26 +0700

    chore: init sonar

--- apps/api/law-svc/src/main.ts
+++ apps/api/law-svc/src/main.ts
@@ -1,10 +1,7 @@
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import {
-  PrismaClientExceptionFilter,
-  ValidationErrorPipe,
-} from '@law-knowledge/shared';
+import { ValidationErrorPipe } from '@law-knowledge/shared';
 import { TcpOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
@@ -19,7 +16,6 @@ async function bootstrap() {
   } as TcpOptions);
 
   app.useGlobalPipes(new ValidationErrorPipe());
-  app.useGlobalFilters(new PrismaClientExceptionFilter());
   app.useLogger(app.get(Logger));
   await app.listen();
 }
--- libs/shared/src/filters/index.ts
+++ libs/shared/src/filters/index.ts
@@ -1,3 +1,2 @@
 export * from './rpc-exception.filter';
 export * from './not-found-exception.filter';
-export * from './prisma-client-exception.filter';
