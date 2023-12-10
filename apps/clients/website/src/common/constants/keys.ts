20fe0e593464985542f065b790d13f703652e5db
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-12-10 11:01:16 +0700

    feat: init modernize branch

--- apps/clients/website/src/common/constants/keys.ts
+++
@@ -1,5 +0,0 @@
-const StorageKeys = {
-  ACCESS_TOKEN: 'X-Access-Token',
-};
-
-export { StorageKeys };
--- apps/clients/website/src/features/SignatureService/components/AccessibilityProps.tsx
+++
@@ -1,6 +0,0 @@
-export default function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
