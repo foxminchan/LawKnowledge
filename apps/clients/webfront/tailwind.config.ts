54663e53243d997fa1bbe1c5275cf21800911acf
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-12-10 11:30:44 +0700

    chore: update package

---
+++ apps/clients/webfront/tailwind.config.ts
@@ -0,0 +1,16 @@
+import { join } from 'path';
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
+
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
